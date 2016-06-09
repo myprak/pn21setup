0f22b6d2023f15a8b3a0dd49e7b6cf5d2ed9ec3e
Ubuntu <ubuntu@ip-172-31-25-176.ap-southeast-1.compute.internal>
2016-06-09 06:54:04 +0000

    deleted files

--- bigdeskSetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/share/elasticsearch/plugins/
-sudo git clone https://github.com/lukas-vlcek/bigdesk
-sudo mkdir _site
-sudo mv bigdesk/* _site
-sudo mv _site bigdesk
-sed -i"" 's/major == 1/major >= 1/' bigdesk/_site/js/store/BigdeskStore.js
