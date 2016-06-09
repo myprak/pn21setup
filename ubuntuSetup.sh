168dff5a73c8a98f634cf6d8422f2645fed4622a
myprak <myprak@gmail.com>
2016-06-09 14:54:11 +0700

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -34,8 +34,3 @@ rm ngrok-stable-linux-amd64.zip
 sudo apt-get install -y nmap
 
 
-# ----21dotco-----needs pass username plus need to store machine wallet retrieval key, plus reboot for docker
-curl https://21.co | sh
-21 join
-21 sell start --all
-
